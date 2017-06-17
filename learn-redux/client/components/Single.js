8ed201ae7e8160a1212d4c415797ad17352a2cd5
James Sullivan <jamesfsullivan5@gmail.com>
2017-06-17 16:39:14 -0600

    Add single photo view.

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import Photo from './Photo';
 
 const Single = React.createClass({
   render() {
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    console.log(i);
+    const post = this.props.posts[i];
+    console.log(post);
     return (
       <div className="single-photo">
-        I am the single
+        <Photo i={i} post={post} {...this.props} />
       </div>
     )
   }
